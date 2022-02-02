b03883299842d3b15da3f70c775438899acca85a
Greg64200 <laumondgregory@gmail.com>
2022-02-02 19:50:27 +0100

    NavBar is done but links are not functional yet and the sign up component button is missing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
-// import Home from "./components/pages/Home";
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NavBar from "./Components/Pages/NavBar";
-// import Services from "./components/pages/Services";
-// import Products from "./components/pages/Products";
-// import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -13,12 +10,6 @@ function App() {
       <div>
         <Router>
           <NavBar />
-          {/* <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/services" component={Services} />
-            <Route path="/products" component={Products} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch> */}
         </Router>
       </div>
     </>

9a04d3dc5ac4bce04bcd999e691811359cc5f5e8
Greg64200 <laumondgregory@gmail.com>
2022-02-02 20:00:15 +0100

    SignUp button has been made and added to the navBar

--- my-app/src/Components/Pages/Navbar.js
+++ my-app/src/Components/Pages/Navbar.js
@@ -31,7 +31,7 @@ function NavBar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             WONDERS
-            <i class="" />
+            <i class="fas fa-globe-americas" id="logo" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -79,58 +79,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-//   return (
-//     <>
-//       <nav className="navbar">
-//         <div className="navbar-container">
-//           <Link to="/" className="navbar-logo" onClick="closeMobileMenu">
-//             Travel
-//             <i class="fab fa-typo3" />
-//           </Link>
-//           <div className="menu-icon" onClick="closeMobileMenu">
-//             <i className={click ? "fas fa-times" : "fas fa-bars"} />
-//           </div>
-//           <ul className={click ? "nav-menu active" : "nav-menu"}>
-//             <li className="nav-item">
-//               <Link to="/" className="nav-links" onClick="closeMobileMenu">
-//                 Home
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link
-//                 to="/services"
-//                 className="nav-links"
-//                 onClick="closeMobileMenu"
-//               >
-//                 Services
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link
-//                 to="/products"
-//                 className="nav-links"
-//                 onClick="closeMobileMenu"
-//               >
-//                 Products
-//               </Link>
-//             </li>
-
-//             <li>
-//               <Link
-//                 to="/sign-up"
-//                 className="nav-links-mobile"
-//                 onClick="closeMobileMenu"
-//               >
-//                 Sign Up
-//               </Link>
-//             </li>
-//           </ul>
-//           {/* {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} */}
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
-
-// export default NavBar;

c730198796cc3c2f59261914e8d9babc9c7a798a
Greg64200 <laumondgregory@gmail.com>
2022-02-03 17:20:40 +0100

    Footer was created

--- my-app/src/Components/Pages/PagesDetail/Home.js
+++ my-app/src/Components/Pages/PagesDetail/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import AdventureSection from "../AdventureSection";
 import HomePictures from "./HomePictures";
+import Footer from "../Footer";
 
 function Home() {
   return (
     <>
       <AdventureSection />
-
       <HomePictures />
+      <Footer />
     </>
   );
 }

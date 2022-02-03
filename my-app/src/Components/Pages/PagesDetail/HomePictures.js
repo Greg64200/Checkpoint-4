48dec3462720fe572ec779c928d2bc7d88227470
Greg64200 <laumondgregory@gmail.com>
2022-02-03 12:44:49 +0100

    HomePictures section was created

---
+++ my-app/src/Components/Pages/PagesDetail/HomePictures.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./HomePictures.css";
+import HomePicturesItem from "./HomePicturesItem";
+
+function HomePictures() {
+  return (
+    <div className="cards">
+      <h1>The most beautiful destinations you could ever dream of !!!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <HomePicturesItem
+              src="Images/img-1.jpg"
+              text="Enjoy beautiful treks in the Vietnamese countryside"
+              label="Trekking"
+              path="/services"
+            />
+            <HomePicturesItem
+              src="Images/img-2.jpg"
+              text="Come sleep under the stars in the Namibian desert"
+              label="Scenery"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <HomePicturesItem
+              src="Images/img-3.jpg"
+              text="Relax in beautiful crystal clear waters of Tahiti"
+              label="Resting"
+              path="/services"
+            />
+            <HomePicturesItem
+              src="Images/img-4.jpg"
+              text="Explore the mystical lands of Japan and its Mount Fuji"
+              label="magic landscape"
+              path="/products"
+            />
+            <HomePicturesItem
+              src="Images/img-5.jpg"
+              text="Connect with nature by visiting the mysterious lands of Iceland"
+              label="Adventure"
+              path="/sign-up"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomePictures;

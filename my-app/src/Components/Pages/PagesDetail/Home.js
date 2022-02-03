2669c54168def787afc45e1629084d1494e11ca4
Greg64200 <laumondgregory@gmail.com>
2022-02-03 10:39:43 +0100

    The Home component has been made

---
+++ my-app/src/Components/Pages/PagesDetail/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdventureSection from "../AdventureSection";
+
+function Home() {
+  return (
+    <>
+      <AdventureSection />
+    </>
+  );
+}
+
+export default Home;
